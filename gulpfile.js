c5b66c1024aac2ffc6c908953d1cbf794b4d4166
Eric Shinn <eric@eric.cx>
2016-06-29 09:34:24 -0400

    Initial React Components

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var config = {
 	paths: {
 		html: './src/*.html',
 		js: './src/**/*.js',
+		images: './src/images/*',
 		css: [
 			'node_modules/bootstrap/dist/css/bootstrap.min.css',
 			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -62,6 +63,18 @@ gulp.task('css', function() {
 		.pipe(connect.reload());
 });
 
+// Migrates images to dist folder
+// Note that I could even optimize my images here
+gulp.task('images', function() {
+	gulp.src(config.paths.images)
+		.pipe(gulp.dest(config.paths.dist + '/images'))
+		.pipe(connect.reload());
+
+	// public favicon
+	gulp.src('./src/favicon.ico')
+		.pipe(gulp.dest(config.paths.dist));
+});
+
 gulp.task('lint', function() {
 	return gulp.src(config.paths.js)
 		.pipe(lint({config: 'eslint.config.json'}))
@@ -74,4 +87,4 @@ gulp.task('watch', function() {
 	gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
-gulp.task('default', ['html', 'css', 'js', 'lint', 'open', 'watch']);
+gulp.task('default', ['html', 'css', 'images', 'js', 'lint', 'open', 'watch']);
